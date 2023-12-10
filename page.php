e6e0edce116ea10017bd6298d3405ab80ddbcd54
cleversamerr <thedev.samer@gmail.com>
2023-12-10 16:09:25 +0200

    renamed some variables and made it more meaningful

--- page.php
+++ page.php
@@ -18,14 +18,14 @@ while (have_posts()) {
 
     <div class="container container--narrow page-section">
         <?php
-        $theParent = wp_get_post_parent_id(get_the_id());
-        if ($theParent) { ?>
+        $parentPage = wp_get_post_parent_id(get_the_id());
+        if ($parentPage) { ?>
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentPage) ?>"><i
                                 class="fa fa-home"
                                 aria-hidden="true"></i> Back
-                        to <?php echo get_the_title($theParent); ?></a> <span
+                        to <?php echo get_the_title($parentPage); ?></a> <span
                             class="metabox__main"><?php the_title(); ?></span>
                 </p>
             </div>
@@ -33,19 +33,19 @@ while (have_posts()) {
         ?>
 
         <?php
-        $testArray = get_pages(array(
+        $childPages = get_pages(array(
             "child_of" => get_the_ID()
         ));
 
-        if ($theParent or $testArray) { ?>
+        if ($parentPage or $childPages) { ?>
             <div class="page-links">
                 <h2 class="page-links__title"><a
-                            href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a>
+                            href="<?php echo get_permalink($parentPage) ?>"><?php echo get_the_title($parentPage) ?></a>
                 </h2>
                 <ul class="min-list">
                     <?php
-                    if ($theParent) {
-                        $findChildrenOf = $theParent;
+                    if ($parentPage) {
+                        $findChildrenOf = $parentPage;
                     } else {
                         $findChildrenOf = get_the_ID();
                     }
