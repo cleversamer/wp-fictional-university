3710a06851c69565b20208391618823fc5be4341
cleversamerr <thedev.samer@gmail.com>
2023-12-11 14:10:41 +0200

    finished building blog

--- header.php
+++ header.php
@@ -25,10 +25,18 @@
                     <li <?php if (is_page("about-us") or wp_get_post_parent_id(get_the_ID()) == 12) echo 'class="current-menu-item"' ?>>
                         <a href="<?php echo site_url("/about-us") ?>">About Us</a>
                     </li>
-                    <li><a href="#">Programs</a></li>
-                    <li><a href="#">Events</a></li>
-                    <li><a href="#">Campuses</a></li>
-                    <li><a href="#">Blog</a></li>
+                    <li>
+                        <a href="#">Programs</a>
+                    </li>
+                    <li>
+                        <a href="#">Events</a>
+                    </li>
+                    <li>
+                        <a href="#">Campuses</a>
+                    </li>
+                    <li <?php if (get_post_type() == "post") echo 'class="current-menu-item"' ?>>
+                        <a href="<?php echo site_url("/blog") ?>">Blog</a>
+                    </li>
                 </ul>
             </nav>
             <div class="site-header__util">
