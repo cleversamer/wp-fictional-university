4734b822e88abc9fa13f310d5721e480d18d57bb
cleversamerr <thedev.samer@gmail.com>
2023-12-12 07:37:43 +0200

    converted a plugin to a must-use plugin

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -18,22 +18,6 @@ function university_features()
 
 add_action("after_setup_theme", "university_features");
 
-function university_post_types()
-{
-    register_post_type("event", array(
-        "public" => true,
-        "labels" => array(
-            "name" => "Events",
-            "add_new_item" => "Add New Event",
-            "edit_item" => "Edit Event",
-            "all_items" => "All Events",
-            "singular_name" => "Event"
-        ),
-        "menu_icon" => "dashicons-calendar"
-    ));
-}
 
-add_action("init", "university_post_types");
 
 ?>
-
