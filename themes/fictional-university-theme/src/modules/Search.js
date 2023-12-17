baf4007c4ede08f9117497a9ee14255eb162b7ed
cleversamerr <thedev.samer@gmail.com>
2023-12-17 15:43:47 +0200

    rewrote javascript search code without jquery and in plain javascript

--- themes/fictional-university-theme/src/modules/Search.js
+++ themes/fictional-university-theme/src/modules/Search.js
@@ -1,228 +1,217 @@
-import $ from "jquery";
+import axios from "axios";
 
 class Search {
+  // 1. describe and create/initiate our object
   constructor() {
     this.addSearchHTML();
-
-    this.openButton = $(".js-search-trigger");
-    this.closeButton = $(".search-overlay__close");
-    this.searchOverlay = $(".search-overlay");
-    this.bodyElement = $("body");
-    this.searchField = $("#search-term");
-    this.resultsElement = $("#search-overlay__results");
-
+    this.resultsDiv = document.querySelector("#search-overlay__results");
+    this.openButton = document.querySelectorAll(".js-search-trigger");
+    this.closeButton = document.querySelector(".search-overlay__close");
+    this.searchOverlay = document.querySelector(".search-overlay");
+    this.searchField = document.querySelector("#search-term");
     this.isOverlayOpen = false;
     this.isSpinnerVisible = false;
-    this.typingTimer = null;
-    this.previousValue = "";
-
+    this.previousValue;
+    this.typingTimer;
     this.events();
   }
 
+  // 2. events
   events() {
-    this.openButton.on("click", this.openOverlay.bind(this));
-    this.closeButton.on("click", this.closeOverlay.bind(this));
-    $(document).on("keyup", this.keyPressDispatcher.bind(this));
-    this.searchField.on("keyup", this.typingLogic.bind(this));
-  }
-
-  typingLogic() {
-    const currentValue = this.searchField.val().trim();
-
-    if (currentValue === this.previousValue) {
-      return;
-    }
-
-    if (!currentValue) {
-      this.resultsElement.html("");
-      this.previousValue = currentValue;
-      this.isSpinnerVisible = false;
-      return;
-    }
-
-    clearTimeout(this.typingTimer);
-
-    if (!this.isSpinnerVisible) {
-      this.resultsElement.html('<div class="spinner-loader"></div>');
-      this.isSpinnerVisible = true;
-    }
-
-    this.typingTimer = setTimeout(this.getResults.bind(this), 750);
-    this.previousValue = currentValue;
-  }
-
-  getResults() {
-    const searchValue = this.searchField.val().trim();
-    const searchURL = `${universityData.root_url}/wp-json/university/v1/search?term=${searchValue}`;
-
-    $.getJSON(searchURL, (data) => {
-      const { generalInfo, professors, programs, campuses, events } = data;
-
-      const html = `
-      <div class="row">
-        <div class="one-third">
-          <h2 class="search-overlay__section-title">General Information</h2>
-          ${
-            generalInfo.length
-              ? `<ul class="link-list min-list">
-              ${generalInfo
-                .map(({ title, permalink, postType, authorName }) =>
-                  `<li>
-                      <a href="${permalink}">${title}</a>
-                      ${postType === "post" ? `by ${authorName}` : ""}
-                    </li>
-                  `.trim()
-                )
-                .join("")}
-                  </ul>`.trim()
-              : "<p>No general information matches that search.</p>"
-          }
-        </div>
-        
-        <div class="one-third">
-          <h2 class="search-overlay__section-title">Programs</h2>
-          ${
-            programs.length
-              ? `<ul class="link-list min-list">
-              ${programs
-                .map(({ title, permalink }) =>
-                  `<li><a href="${permalink}">${title}</a></li>`.trim()
-                )
-                .join("")}
-                  </ul>`.trim()
-              : `<p>
-                No programs match that search.
-                <a href="${universityData.root_url}/programs">View all programs</a>
-              </p>`
-          }
-          
-          <h2 class="search-overlay__section-title">Professors</h2>
-          ${
-            professors.length
-              ? `<ul class="professor-cards">
-              ${professors
-                .map(({ title, permalink, image }) =>
-                  `
-                  <li class="professor-card__list-item">
-                      <a class="professor-card" href="${permalink}">
-                          <img src="${image}" class="professor-card__image" />
-
-                          <span class="professor-card__name">
-                          ${title}
-                          </span>
-                      </a>
-                  </li>
-                  `.trim()
-                )
-                .join("")}
-                  </ul>`.trim()
-              : "<p>No professors match that search.</p>"
-          }
-        </div>
-        
-        <div class="one-third">
-          <h2 class="search-overlay__section-title">Campuses</h2>
-          ${
-            campuses.length
-              ? `<ul class="link-list min-list">
-              ${campuses
-                .map(({ title, permalink }) =>
-                  `<li><a href="${permalink}">${title}</a></li>`.trim()
-                )
-                .join("")}
-                  </ul>`.trim()
-              : `<p>
-              No campuses match that search.
-              <a href="${universityData.root_url}/campuses">View all campuses</a>
-            </p>`
-          }
-
-          <h2 class="search-overlay__section-title">Events</h2>
-          ${
-            events.length
-              ? `${events
-                  .map(({ title, permalink, month, day, description }) =>
-                    `
-                    <div class="event-summary">
-                        <a class="event-summary__date t-center" href="${permalink}">
-                            <span class="event-summary__month">${month}</span>
-                            <span class="event-summary__day">${day}</span>
-                        </a>
-                    
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny">
-                                <a href="${permalink}">${title}</a>
-                            </h5>
-                    
-                            <p>
-                                ${description}
-                                <a href="${permalink}" class="nu gray">Learn more</a>
-                            </p>
-                        </div>
-                    </div>
-                  `.trim()
-                  )
-                  .join("")}
-                `.trim()
-              : `<p>
-              No events match that search.
-              <a href="${universityData.root_url}/events">View all events</a>
-            </p>`
-          }
-        </div>
-      </div>
-      `;
-
-      this.resultsElement.html(html);
-      this.isSpinnerVisible = false;
+    this.openButton.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.openOverlay();
+      });
     });
+
+    this.closeButton.addEventListener("click", () => this.closeOverlay());
+    document.addEventListener("keydown", (e) => this.keyPressDispatcher(e));
+    this.searchField.addEventListener("keyup", () => this.typingLogic());
   }
 
-  keyPressDispatcher(event) {
+  // 3. methods (function, action...)
+  typingLogic() {
+    if (this.searchField.value != this.previousValue) {
+      clearTimeout(this.typingTimer);
+
+      if (this.searchField.value) {
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.innerHTML = '<div class="spinner-loader"></div>';
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750);
+      } else {
+        this.resultsDiv.innerHTML = "";
+        this.isSpinnerVisible = false;
+      }
+    }
+
+    this.previousValue = this.searchField.value;
+  }
+
+  async getResults() {
+    try {
+      const response = await axios.get(
+        universityData.root_url +
+          "/wp-json/university/v1/search?term=" +
+          this.searchField.value
+      );
+      const results = response.data;
+      this.resultsDiv.innerHTML = `
+        <div class="row">
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">General Information</h2>
+            ${
+              results.generalInfo.length
+                ? '<ul class="link-list min-list">'
+                : "<p>No general information matches that search.</p>"
+            }
+              ${results.generalInfo
+                .map(
+                  (item) =>
+                    `<li><a href="${item.permalink}">${item.title}</a> ${
+                      item.postType == "post" ? `by ${item.authorName}` : ""
+                    }</li>`
+                )
+                .join("")}
+            ${results.generalInfo.length ? "</ul>" : ""}
+          </div>
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">Programs</h2>
+            ${
+              results.programs.length
+                ? '<ul class="link-list min-list">'
+                : `<p>No programs match that search. <a href="${universityData.root_url}/programs">View all programs</a></p>`
+            }
+              ${results.programs
+                .map(
+                  (item) =>
+                    `<li><a href="${item.permalink}">${item.title}</a></li>`
+                )
+                .join("")}
+            ${results.programs.length ? "</ul>" : ""}
+
+            <h2 class="search-overlay__section-title">Professors</h2>
+            ${
+              results.professors.length
+                ? '<ul class="professor-cards">'
+                : `<p>No professors match that search.</p>`
+            }
+              ${results.professors
+                .map(
+                  (item) => `
+                <li class="professor-card__list-item">
+                  <a class="professor-card" href="${item.permalink}">
+                    <img class="professor-card__image" src="${item.image}">
+                    <span class="professor-card__name">${item.title}</span>
+                  </a>
+                </li>
+              `
+                )
+                .join("")}
+            ${results.professors.length ? "</ul>" : ""}
+
+          </div>
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">Campuses</h2>
+            ${
+              results.campuses.length
+                ? '<ul class="link-list min-list">'
+                : `<p>No campuses match that search. <a href="${universityData.root_url}/campuses">View all campuses</a></p>`
+            }
+              ${results.campuses
+                .map(
+                  (item) =>
+                    `<li><a href="${item.permalink}">${item.title}</a></li>`
+                )
+                .join("")}
+            ${results.campuses.length ? "</ul>" : ""}
+
+            <h2 class="search-overlay__section-title">Events</h2>
+            ${
+              results.events.length
+                ? ""
+                : `<p>No events match that search. <a href="${universityData.root_url}/events">View all events</a></p>`
+            }
+              ${results.events
+                .map(
+                  (item) => `
+                <div class="event-summary">
+                  <a class="event-summary__date t-center" href="${item.permalink}">
+                    <span class="event-summary__month">${item.month}</span>
+                    <span class="event-summary__day">${item.day}</span>  
+                  </a>
+                  <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="${item.permalink}">${item.title}</a></h5>
+                    <p>${item.description} <a href="${item.permalink}" class="nu gray">Learn more</a></p>
+                  </div>
+                </div>
+              `
+                )
+                .join("")}
+
+          </div>
+        </div>
+      `;
+      this.isSpinnerVisible = false;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  keyPressDispatcher(e) {
     if (
-      event.keyCode == 83 &&
+      e.keyCode == 83 &&
       !this.isOverlayOpen &&
-      !$("input, textarea").is(":focus")
+      document.activeElement.tagName != "INPUT" &&
+      document.activeElement.tagName != "TEXTAREA"
     ) {
       this.openOverlay();
     }
 
-    if (event.keyCode == 27 && this.isOverlayOpen) {
+    if (e.keyCode == 27 && this.isOverlayOpen) {
       this.closeOverlay();
     }
   }
 
   openOverlay() {
-    this.searchOverlay.addClass("search-overlay--active");
-    this.bodyElement.addClass("body-no-scroll");
-    this.searchField.val("");
-    this.resultsElement.html("");
+    this.searchOverlay.classList.add("search-overlay--active");
+    document.body.classList.add("body-no-scroll");
+    this.searchField.value = "";
     setTimeout(() => this.searchField.focus(), 301);
+    console.log("our open method just ran!");
     this.isOverlayOpen = true;
+    return false;
   }
 
   closeOverlay() {
-    this.searchOverlay.removeClass("search-overlay--active");
-    this.bodyElement.removeClass("body-no-scroll");
+    this.searchOverlay.classList.remove("search-overlay--active");
+    document.body.classList.remove("body-no-scroll");
+    console.log("our close method just ran!");
     this.isOverlayOpen = false;
   }
 
   addSearchHTML() {
-    $("body").append(`
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
       <div class="search-overlay">
         <div class="search-overlay__top">
-            <div class="container">
-                <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-                <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off" />
-                <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
-            </div>
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        
+        <div class="container">
+          <div id="search-overlay__results"></div>
         </div>
 
-        <div class="container">
-            <div id="search-overlay__results"></div>
-        </div>
-    </div>
-    `);
+      </div>
+    `
+    );
   }
 }
 
